3562d8756a78a8b5cd7b95265b204495e800754a
Inga-Z <z_inga@mail.ru>
2019-12-17 21:52:55 +0300

    SecondStep

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('worked');
+const express = require('express');
+const cors = require('cors');
+
+let nextId = 1;
+let posts = [
+    {id: nextId++, content: 'First post'},
+    {id: nextId++, content: 'Second post'},
+];
+
+const server = express();
+server.subscribe(express.json());
+server.use(cors());
+
+server.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+server.post('/posts', (req, res) => {
+
+});
+
+server.listen(process.env.PORT || 9999);
+
